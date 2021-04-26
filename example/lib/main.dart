b943b38608e321e7379c726ee0a8268c21520ad8
Ritick Saha <RitickSaha@users.noreply.github.com>
2021-04-26 13:01:20 +0530

    Minor Changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -86,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         alignment: Alignment.center,
                         child: Stack(
                           children: <Widget>[
-                            Center(child: Container()),
+                            // Center(child: Container()),
                             Positioned(
                               bottom:
                                   MediaQuery.of(context).size.height * 0.3 - 70,
@@ -190,183 +190,189 @@ class _MyHomePageState extends State<MyHomePage> {
 
 // ignore: non_constant_identifier_names
 GlassCard(var context) {
-  return GlassmorphicContainer(
-      width: MediaQuery.of(context).size.width * 0.9 - 20,
-      height: MediaQuery.of(context).size.height * 0.4 - 20,
-      borderRadius: 35,
-      blur: 14,
-      alignment: Alignment.bottomCenter,
-      border: 2,
-      linearGradient: LinearGradient(
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-        colors: [
-          Color(0xFF0FFFF).withOpacity(0.2),
-          Color(0xFF0FFFF).withOpacity(0.2),
-        ],
-      ),
-      borderGradient: LinearGradient(
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-        colors: [
-          Color(0xFF0FFFF).withOpacity(1),
-          Color(0xFFFFFFF),
-          Color(0xFF0FFFF).withOpacity(1),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          SizedBox(height: MediaQuery.of(context).size.height * 0.001),
-          Image.network(
-            "https://pub.dev/static/img/pub-dev-logo-2x.png?hash=umitaheu8hl7gd3mineshk2koqfngugi",
-            scale: 1.7,
+  return Column(
+    mainAxisSize: MainAxisSize.max,
+    children: [
+      GlassmorphicContainer(
+          width: MediaQuery.of(context).size.width * 0.9 - 20,
+          height: MediaQuery.of(context).size.height * 0.4 - 20,
+          borderRadius: 35,
+          blur: 14,
+          alignment: Alignment.bottomCenter,
+          border: 2,
+          linearGradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF0FFFF).withOpacity(0.2),
+              Color(0xFF0FFFF).withOpacity(0.2),
+            ],
           ),
-          InkWell(
-            onTap: () {
-              // launchPubDev();
-            },
-            child: Text(
-              pac,
-              style: TextStyle(
-                fontFamily: 'RobotoMono',
-                fontSize: 25.0,
-                color: Colors.white,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
+          borderGradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF0FFFF).withOpacity(1),
+              Color(0xFFFFFFF),
+              Color(0xFF0FFFF).withOpacity(1),
+            ],
           ),
-          Text(
-            "Published on $date",
-            style: TextStyle(
-              fontFamily: 'RobotoMono',
-              fontSize: 18.0,
-              fontStyle: FontStyle.italic,
-              color: Colors.white,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          Text(
-            "Published by Ritick Saha\n(The Flutter Foundry)",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontFamily: 'RobotoMono',
-              fontStyle: FontStyle.italic,
-              fontSize: 18.0,
-              color: Colors.white,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.001),
-          Row(
+          child: Column(
+            key: UniqueKey(),
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              RichText(
-                text: TextSpan(
-                  text: '$likes',
+              SizedBox(height: MediaQuery.of(context).size.height * 0.001),
+              Image.network(
+                "https://pub.dev/static/img/pub-dev-logo-2x.png?hash=umitaheu8hl7gd3mineshk2koqfngugi",
+                scale: 1.7,
+              ),
+              InkWell(
+                onTap: () {
+                  // launchPubDev();
+                },
+                child: Text(
+                  pac,
                   style: TextStyle(
                     fontFamily: 'RobotoMono',
-                    fontSize: 35.0,
+                    fontSize: 25.0,
                     color: Colors.white,
                     fontWeight: FontWeight.w900,
                   ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: '\nLikes',
-                      style: TextStyle(
-                        fontFamily: 'RobotoMono',
-                        fontSize: 15.0,
-                        color: Colors.white60,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    )
-                  ],
                 ),
               ),
-              RichText(
-                text: TextSpan(
-                  text: '$gotpoints',
-                  style: TextStyle(
-                    fontFamily: 'RobotoMono',
-                    fontSize: 35.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900,
-                  ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: '/$totalPoints',
+              Text(
+                "Published on $date",
+                style: TextStyle(
+                  fontFamily: 'RobotoMono',
+                  fontSize: 18.0,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                "Published by Ritick Saha\n(The Flutter Foundry)",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'RobotoMono',
+                  fontStyle: FontStyle.italic,
+                  fontSize: 18.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.001),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: '$likes',
                       style: TextStyle(
                         fontFamily: 'RobotoMono',
-                        fontSize: 15.0,
+                        fontSize: 35.0,
                         color: Colors.white,
                         fontWeight: FontWeight.w900,
                       ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: '\nLikes',
+                          style: TextStyle(
+                            fontFamily: 'RobotoMono',
+                            fontSize: 15.0,
+                            color: Colors.white60,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
                     ),
-                    TextSpan(
-                      text: '\n    Pub Point',
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: '$gotpoints',
                       style: TextStyle(
                         fontFamily: 'RobotoMono',
-                        fontSize: 15.0,
-                        color: Colors.white60,
-                        fontWeight: FontWeight.w500,
+                        fontSize: 35.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900,
                       ),
-                    )
-                  ],
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: '/$totalPoints',
+                          style: TextStyle(
+                            fontFamily: 'RobotoMono',
+                            fontSize: 15.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '\n    Pub Point',
+                          style: TextStyle(
+                            fontFamily: 'RobotoMono',
+                            fontSize: 15.0,
+                            color: Colors.white60,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: ' $popularity%',
+                      style: TextStyle(
+                        fontFamily: 'RobotoMono',
+                        fontSize: 35.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: '\nPopularity',
+                          style: TextStyle(
+                            fontFamily: 'RobotoMono',
+                            fontSize: 15.0,
+                            color: Colors.white60,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.001),
+              Text(
+                "Small Package Discription:",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'RobotoMono',
+                  fontStyle: FontStyle.italic,
+                  fontSize: 18.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
                 ),
               ),
-              RichText(
-                text: TextSpan(
-                  text: ' $popularity%',
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  "$details",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                     fontFamily: 'RobotoMono',
-                    fontSize: 35.0,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 18.0,
                     color: Colors.white,
-                    fontWeight: FontWeight.w900,
+                    fontWeight: FontWeight.w200,
                   ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: '\nPopularity',
-                      style: TextStyle(
-                        fontFamily: 'RobotoMono',
-                        fontSize: 15.0,
-                        color: Colors.white60,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    )
-                  ],
                 ),
               ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.001),
             ],
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.001),
-          Text(
-            "Small Package Discription:",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontFamily: 'RobotoMono',
-              fontStyle: FontStyle.italic,
-              fontSize: 18.0,
-              color: Colors.white,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal:8),
-            child: Text(
-              "$details",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'RobotoMono',
-                fontStyle: FontStyle.italic,
-                fontSize: 18.0,
-                color: Colors.white,
-                fontWeight: FontWeight.w200,
-              ),
-            ),
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.001),
-        ],
-      ));
+          )),
+    ],
+  );
 }
 
 class Body2 extends StatelessWidget {
@@ -464,6 +470,7 @@ class Body2 extends StatelessWidget {
     );
   }
 }
+
 Future<int> maintest() async {
   final response = await http.Client()
       .get(Uri.parse("https://pub.dev/packages/glassmorphism/score"));
