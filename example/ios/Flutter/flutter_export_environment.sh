0259ba002373200b1df986f1cb6578d18fba2025
Ritick Saha <RitickSaha@users.noreply.github.com>
2021-04-26 13:00:41 +0530

    Generated Files

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Flutter Packages Creation\Glassmorphism\example\glassmorphic_example"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter Packages Creation\Glassmorphism\Git Repository\glassmorphism\example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
